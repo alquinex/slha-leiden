c5ff782b7b4b41969e7ab928a263b812bc9d61c9
Alusine Q. Sesay <alusineq.sesay@Alusines-MacBook-Pro.local>
2021-01-08 17:12:15 +0100

    menu links

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,19 +1,45 @@
-import { Box, Flex, Image, useBreakpointValue } from '@chakra-ui/react'
 import React from 'react'
+import {
+  Box,
+  Flex,
+  Image,
+  useBreakpointValue,
+  Link as ChakraLink,
+} from '@chakra-ui/react'
+import { Link } from 'gatsby'
 
 import Container from '@/components/shared/container'
 
 import logoFull from '@/images/logo-full.png'
 import logo from '@/images/logo.png'
 
+const NavLink = ({ children, ...props }) => (
+  <ChakraLink as={Link} px={2} color='primary' {...props}>
+    {children}
+  </ChakraLink>
+)
+
 const Header = () => {
+  const [show, setShow] = React.useState(false)
+  const handleToggle = () => setShow(!show)
+
   const logoSrc = useBreakpointValue([logo, logoFull])
   return (
     <Box boxShadow='md'>
       <Container fluid>
-        <Flex py={4}>
+        <Flex py={4} align='center' justify='space-between'>
           <Image src={logoSrc} h='64px' />
-          {/* <Image src={logoFull} h={['20px', '40px', '64px']} /> */}
+          <Box
+            display={{ sm: show ? 'block' : 'none', md: 'flex' }}
+            padding='1.5rem'
+            fontWeight='bold'
+            onClick={handleToggle}
+          >
+            <NavLink>Home</NavLink>
+            <NavLink>About Us </NavLink>
+            <NavLink>What We Do</NavLink>
+            <NavLink>Contact Us</NavLink>
+          </Box>
         </Flex>
       </Container>
     </Box>
